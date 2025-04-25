10a22a4a3e2e71a2868f54fa233f433e3db99c29
Mithila Khan <mithilakhan082@gmail.com>
2025-04-25 12:23:27 +0600

    dsf

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "**",
-        },
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-      ],
-    },
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias['pdfjs-dist/build/pdf'] = 'pdfjs-dist/legacy/build/pdf';
+    config.resolve.alias['pdfjs-dist/build/pdf.worker.entry'] = 'pdfjs-dist/legacy/build/pdf.worker.entry';
+    return config;
+  },
 };
 
 export default nextConfig;
